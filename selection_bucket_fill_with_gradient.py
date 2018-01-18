c5bbd68d7b19864ecee203fa078e9ff87364ad3a
Vladimir Elistratov <vvoovv@users.noreply.github.com>
2018-01-18 22:16:09 +0300

    Create selection_bucket_fill_with_gradient.py

---
+++ selection_bucket_fill_with_gradient.py
@@ -0,0 +1,12 @@
+def selection_bucket_fill_with_gradient(numPixels=4.):
+    img = gimp.image_list()[0]
+    drw = pdb.gimp_image_active_drawable(img)
+    pdb.gimp_edit_copy(img.active_layer)
+    floating_sel = pdb.gimp_edit_paste(drw, False)
+    pdb.gimp_floating_sel_to_layer(floating_sel)
+    pdb.gimp_selection_layer_alpha(img.active_layer)
+    pdb.gimp_selection_feather(img, numPixels)
+    drw = pdb.gimp_image_active_drawable(img)
+    pdb.gimp_bucket_fill(drw, FG_BUCKET_FILL, NORMAL_MODE, 100, 0, 0, 0, 0)
+    pdb.gimp_image_lower_layer(img, img.active_layer)
+    pdb.gimp_image_merge_down(img, img.active_layer, EXPAND_AS_NECESSARY)
